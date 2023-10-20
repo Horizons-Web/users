a97f13abe83ca152d84e53f8449c030571243c7c
Joaquin Reyero <48756494+joaquinreyero@users.noreply.github.com>
2023-10-20 03:39:40 -0300

    eliminando init.sql

--- init_script/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE users;
-CREATE USER trippi WITH ENCRYPTED PASSWORD 'admin';
-GRANT ALL PRIVILEGES ON DATABASE users TO trippi;
-\c users trippi;
-GRANT ALL ON SCHEMA public TO trippi;
-
